27d9819c350ece16ce0d9932f190c9a57c470edf
Andrew Poon <akpoon@gmail.com>
2019-06-21 22:47:30 -0700

    working on button

--- src/components/App.js
+++ src/components/App.js
@@ -9,58 +9,25 @@ import MovieList from "./MovieList.js"
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {searchTerm: ""}
+    this.state = {searchTerm: "", insertMovie: ""}
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  // handleChange (event) {
-  //   // console.log("sup", event.target.value);
-  //   var userInput = event.target.value;
-  //   // exampleMovies.map((movie) => {
-  //   //   if(movie.title)
-  //   // }
-  //   const result = exampleMovies.filter(word => word.title.length > 8);
-  //   // const result = words.filter(word => word.length > 6);
-
-
-  //   console.log("hey now", result);
-  //   // console.log(event.target.value, exampleMovies);
-  //   this.setState({value: event.target.value});
-  // }
-
   handleInputChange({target}) {
     const {name, value} = target;
-    // console.log(name);
-    // if(exampleMovies[0].title.contains(value)){
-    //   console.log("it it a MATCHHHH!")
-    // }
-    // console.log("the value is", value); 
-
-    // console.log(exampleMovies[0].title);
-    // var test = exampleMovies[0].title;
-    // if (test.includes('mean')) {
-    //   console.log("ITS A MATCHHHHH")
-    // }
-    // console.log('test is', test);
-
-    var filteredMovies;
-    exampleMovies.map((movie) => {
-      filteredMovies = exampleMovies.filter(movie => movie.title.toLowerCase().includes(value.toLowerCase()));
-    });
-    console.log(filteredMovies);
-    
-    // test.includes(value)? console.log('is') : console.log('is not');
-
     this.setState({
       [name]: value,
     });
-    console.log(this.state);
+    console.log('this is the stttate', this.state);
   }
+
+
+
   //create a filter between state and files
 
   render () {
 
-    const {searchTerm} = this.state;
+    const {searchTerm, insertMovie} = this.state;
     return (
       <div>
         <table>
@@ -70,7 +37,12 @@ class App extends React.Component {
             </tr>
             <tr>
               <td>
-              {/* <Test text = {this.state}/> */}
+              <div className = "searchbar">
+                <form>
+                  <input type="text" placeholder="Add movie title here" value={insertMovie} onChange={this.handleInputChange} name="insertMovie"/>
+                  <input type="submit" value="Add" />
+                </form>
+              </div>
               <div className = "searchbar">
                 <form>
                   <input type="text" placeholder="Search..." value={searchTerm} onChange={this.handleInputChange} name="searchTerm"/>
